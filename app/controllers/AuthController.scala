b8e2b7f385a65ef1bfbfcb9cf275ae22b9240c4c
roytiger81102 <roytiger81102@gmail.com>
2018-11-05 11:10:39 +0900

    ログイン機能追加

--- app/controllers/AuthController.scala
+++ app/controllers/AuthController.scala
@@ -6,22 +6,31 @@ import javax.inject.Inject
 import jp.t2v.lab.play2.auth.LoginLogout
 import play.api.data.Form
 import play.api.data.Forms._
-import play.api.i18n.I18nSupport
+import play.api.i18n.{I18nSupport, Messages}
+import play.api.libs.typedmap.TypedKey
 import play.api.mvc._
 import services.UserService
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
-@Singleton
+/**
+  * ログイン認証を行うコントローラクラス
+  * @param userService
+  * @param components
+  * @param ec
+  */
 class AuthController @Inject()(
   val userService: UserService,
   components: ControllerComponents
-  )(implicit ec: ExecutionContext)
+)(implicit ec: ExecutionContext)
   extends AbstractController(components)
   with I18nSupport
   with AuthConfigSupport
   with LoginLogout {
 
+  /**
+    * ログイン情報の受け渡しを行うフォーム
+    */
   private val loginForm: Form[Login] = Form {
     mapping(
       "email" -> email,
@@ -29,10 +38,63 @@ class AuthController @Inject()(
     )(Login.apply)(Login.unapply).verifying("AuthFailed", form => authenticate(form.email, form.password).isDefined)
   }
 
-  def login: Action[AnyContent] = ???
+  /**
+    * ログインセッションを記憶するフォーム
+    */
+  private val rememberMeForm: Form[Boolean] = Form {
+    "rememberme" -> boolean
+  }
 
-  def logout: Action[AnyContent] = ???
+  /**
+    * 初期表示
+    * @return
+    */
+  def index: Action[AnyContent] = Action { implicit request =>
+    Ok(
+      views.html.login(loginForm, rememberMeForm.fill(request.session.get("rememberme").exists("true" ==)))
+    )
+  }
 
+  /**
+    * ログイン処理を行う
+    * @return
+    */
+  def login: Action[AnyContent] = {
+    Action.async { implicit request =>
+      val rememberMe = rememberMeForm.bindFromRequest()
+      loginForm.bindFromRequest.fold(
+        formWithErrors => Future.successful(BadRequest(views.html.login(formWithErrors, rememberMe))),
+        {
+          login =>
+            val req = request.addAttr(TypedKey[Boolean]("rememberme"), rememberMe.get)
+            markLoggedIn(login.email)(req, ec) {
+              Redirect(routes.HomeController.index())
+                .withSession("rememberme" -> rememberMe.get.toString)
+                .flashing("success" -> Messages("LoggedIn"))
+            }
+        }
+      )
+    }
+  }
+
+  /**
+    * ログアウト処理を行う
+    * @return
+    */
+  def logout: Action[AnyContent] = Action.async { implicit request =>
+    markLoggedOut()(request, ec) {
+      Redirect(routes.HomeController.index())
+        .flashing("success" -> Messages("LoggedOut"))
+        .removingFromSession("rememberme")
+    }
+  }
+
+  /**
+    * メールアドレスからユーザー情報を取得し入力されたパスワードが一致するかチェックする
+    * @param email メールアドレス
+    * @param password パスワード
+    * @return ユーザー情報が存在する場合:ユーザー情報/存在しない場合:None
+    */
   private def authenticate(email: String, password: String): Option[User] = {
     userService
       .findByEmail(email)
--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,24 +1,28 @@
 package controllers
 
 import javax.inject._
-
+import jp.t2v.lab.play2.auth.OptionalAuthElement
 import play.api.i18n.I18nSupport
 import play.api.mvc._
+import services.UserService
 
 /**
   * トップページコントローラクラス
   * @param components
   */
 @Singleton
-class HomeController @Inject()(val components: ControllerComponents)
-  extends AbstractController(components) with I18nSupport {
+class HomeController @Inject()(val userService: UserService, components: ControllerComponents)
+  extends AbstractController(components)
+  with I18nSupport
+  with AuthConfigSupport
+  with OptionalAuthElement {
 
   /**
     * 初期表示
     * @return
     */
-  def index() = Action { implicit request =>
-    Ok(views.html.index())
+  def index() = StackAction { implicit request =>
+    Ok(views.html.index(loggedIn))
   }
 
 }
